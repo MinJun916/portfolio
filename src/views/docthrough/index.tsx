615067b56efa02ce16db1102edf476a21af6ce25
MinJun916 <ed125248@gmail.com>
2026-02-13 17:17:04 +0900

    feat: add DOCTHROUGH_SUMMARY for project overview and integrate into ProjectDocthroughPage

--- src/views/docthrough/index.tsx
+++ src/views/docthrough/index.tsx
@@ -1,5 +1,12 @@
 import ProjectHero from '@/components/projects/projectHero';
+import ProjectOverview from '@/components/projects/projectOverview';
+import { DOCTHROUGH_SUMMARY } from '@/lib/summary/docthrough';
 
-const ProjectDocthroughPage = () => <ProjectHero projectName="docthrough" />;
+const ProjectDocthroughPage = () => (
+  <>
+    <ProjectHero projectName="docthrough" />
+    <ProjectOverview overview={DOCTHROUGH_SUMMARY.overview} />
+  </>
+);
 
 export default ProjectDocthroughPage;
