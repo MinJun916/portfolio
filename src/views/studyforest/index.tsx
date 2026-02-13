04a4e86fd4f81d273e1469741d8da031f0c4e7fc
MinJun916 <ed125248@gmail.com>
2026-02-13 18:09:42 +0900

    feat: add STUDYFOREST_SUMMARY for project overview and integrate into ProjectStudyforestPage

--- src/views/studyforest/index.tsx
+++ src/views/studyforest/index.tsx
@@ -1,5 +1,12 @@
 import ProjectHero from '@/components/projects/projectHero';
+import ProjectOverview from '@/components/projects/projectOverview';
+import { STUDYFOREST_SUMMARY } from '@/lib/summary/studyforest';
 
-const ProjectStudyforestPage = () => <ProjectHero projectName="studyforest" />;
+const ProjectStudyforestPage = () => (
+  <>
+    <ProjectHero projectName="studyforest" />
+    <ProjectOverview overview={STUDYFOREST_SUMMARY.overview} />
+  </>
+);
 
 export default ProjectStudyforestPage;
