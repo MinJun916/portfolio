49cfcd09bfb049b95aba7f7a4525df28f09addfe
MinJun916 <ed125248@gmail.com>
2026-02-13 17:22:37 +0900

    feat: expand DOCTHROUGH_SUMMARY with detailed contributions and integrate into ProjectDocthroughPage

--- src/views/docthrough/index.tsx
+++ src/views/docthrough/index.tsx
@@ -1,3 +1,5 @@
+import ProjectContribution from '@/components/projects/projectContribution';
+import ProjectFooter from '@/components/projects/projectFooter';
 import ProjectHero from '@/components/projects/projectHero';
 import ProjectOverview from '@/components/projects/projectOverview';
 import { DOCTHROUGH_SUMMARY } from '@/lib/summary/docthrough';
@@ -6,6 +8,8 @@ const ProjectDocthroughPage = () => (
   <>
     <ProjectHero projectName="docthrough" />
     <ProjectOverview overview={DOCTHROUGH_SUMMARY.overview} />
+    <ProjectContribution contribution={DOCTHROUGH_SUMMARY.contribution} />
+    <ProjectFooter />
   </>
 );
 
